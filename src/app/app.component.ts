059584e3c4743d25e26a7bec3f98939961ddcc63
JonArbell <138678861+JonArbell@users.noreply.github.com>
2025-02-06 09:59:20 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ObjectiveComponent } from './objective/objective.component';
+import { InformationComponent } from './information/information.component';
+import { EducationComponent } from './education/education.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,InformationComponent,ObjectiveComponent,EducationComponent,ExperienceComponent,ProjectsComponent, SkillsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    theme: {
+      extend: {
+        spacing: {
+          'underline-offset': '10px', // Customize this value as needed
+        },
+      },
+    },
+  }
+  
